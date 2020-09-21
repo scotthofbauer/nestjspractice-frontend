502865ee8a9ef86165bfe67ef5c4d2b737577605
ejohn156 <eric.johnson@levvel.io>
2020-09-21 12:17:42 -0400

    set up useEffect hook and am able to display response within the feed component on the web page

--- src/components/feed/index.tsx
+++ src/components/feed/index.tsx
@@ -1,35 +1,42 @@
 import * as React from 'react'
+import { useEffect, useState } from 'react';
 import {IPost} from '../../schemas/Post.d'
 const Feed: React.FC = () => {
 
+    const emptyList: IPost[] = []
+    const getPostsData = () =>  {
+        return fetch('http://localhost:3001/posts').then(response => {
+          if (response.status !== 200) {
+              throw new Error(`${response.status} ${response.statusText}`);
+            }
+            return response.json();
+          });
+      }
     const useFeedService = () => {
-        return {
-            posts: [{
-                id: 1,
-                test: "placeholder"
-            },
-            {
-                id: 2,
-                test: "second placeholder"
-            }],
+        const [state, setState] = useState({
+            posts: emptyList,
             error: null,
-            isPending: false
-        }
+            isPending: true
+          });
+        useEffect(() => {
+            getPostsData()
+              .then(value => setState({ posts: value, error: null, isPending: false }))
+              .catch(error => setState({ ...state, error: error.toString(), isPending: false }));
+          }, emptyList);
+          const { posts, error, isPending } = state;
+          return [posts,error,isPending]
     }
-    const { posts, error, isPending } = useFeedService();
 
+    const [posts, error,isPending] = useFeedService();
+    console.log([posts, error,isPending])
 
     return (
         <div>
         {error && <pre>ERROR! {error}...</pre>}
         {isPending && <pre>LOADING...</pre>}
-        {posts.map((post :IPost) => {
-            return(
-                <div key={post.id}>
-                <div><pre>{JSON.stringify(post, null, 2)}</pre></div>
-                </div>
-            )
-        })}
+        {
+            <div><pre>{JSON.stringify(posts, null, 2)}</pre></div>
+        }
     </div>
     )
 };
--- src/schemas/Post.d.tsx
+++ src/schemas/Post.d.tsx
@@ -1,8 +1,11 @@
 import { schema } from 'normalizr';
 import resources from './resources'
 export interface IPost {
-    id: number;
-    test: string;
+  id: number,
+  title: string,
+  message: string,
+  categories: [],
+  favorites: []
   }
   
   export const PostSchema = new schema.Entity<IPost>(resources.post);
