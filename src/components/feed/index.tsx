f654028a1b252b788b723f840cd4a16bb2ddb58b
ejohn156 <eric.johnson@levvel.io>
2020-09-22 13:54:48 -0400

    got refresh feed button working properly

--- src/components/feed/index.tsx
+++ src/components/feed/index.tsx
@@ -1,25 +1,31 @@
 import * as React from 'react'
-import {IPost} from '../../schemas/Post.d'
+import { useState } from 'react';
+import { IPost } from '../../schemas/Post.d'
 import FeedItem from './FeedItem/feedItem';
 import useFeedService from './services/feedService';
 
 const Feed: React.FC = () => {
-    
-    
-     const state = useFeedService();
-     console.log(state)
+    const emptyList: IPost[] = []
+    const [state, setState] = useState({
+        posts: emptyList,
+        error: null,
+        isPending: true
+    });
+    const renderedState = useFeedService(state, setState)
+    console.log(state)
     return (
         <div>
-        {state.error && <pre>ERROR! {state.error}...</pre>}
-        {state.isPending && <pre>LOADING...</pre>}
-        {state.posts.map((post :IPost) => {
-            return(
-                <div key={post.id}> 
-                <FeedItem {...post}/>
-                 </div>
-            )
-        })}
-    </div>
+            <button onClick={() => setState({posts: renderedState.posts, error: renderedState.error,isPending: true})}>refresh</button>
+            {renderedState.error && <pre>ERROR! {state.error}...</pre>}
+            {renderedState.isPending && <pre>LOADING...</pre>}
+            {renderedState.posts.map((post: IPost) => {
+                return (
+                    <div key={post.id}>
+                        <FeedItem {...post} />
+                    </div>
+                )
+            })}
+        </div>
     )
 };
 
--- src/components/feed/services/feedService.tsx
+++ src/components/feed/services/feedService.tsx
@@ -1,32 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { IPost } from "../../../schemas/Post.d";
-const useFeedService = () => {
-const emptyList: IPost[] = []
-const [state, setState] = useState({
-  posts: emptyList,
+const useFeedService = (state: {
+  posts: IPost[],
   error: null,
-  isPending: true
-});
-    
-    const getPostsData = async () =>  {
-      
-        return fetch('http://localhost:3001/posts').then(async(response) => {
-          if (response.status !== 200) {
-              throw new Error(`${response.status} ${response.statusText}`);
-            }
-            return await response.json();
-          });
-      }
-      
-    const FeedService = () => {    
+  isPending: boolean
+}, setState: Function) => {
         useEffect(() => {
+          const getPostsData = () =>  {  
+            return fetch('http://localhost:3001/posts').then(async(response) => {
+              if (response.status !== 200) {
+                  throw new Error(`${response.status} ${response.statusText}`);
+                }
+                return await response.json()
+              });
+          }
+          if(state.isPending){
             getPostsData()
               .then(value => setState({ posts: value, error: null, isPending: false }))
               .catch(error => setState({ ...state, error: error.toString(), isPending: false }));
-          }, []);
+          }
+          }, [state, setState]);
           return state
-    
-}
-return FeedService()
-}
+        }
 export default useFeedService
